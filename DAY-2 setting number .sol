97d5890405546288a64ec8615ab2fb65a30cfea6
Shubham Singh <77406420+Shubhamjatara@users.noreply.github.com>
2025-06-23 21:21:06 +0530

    Update DAY-2 setting number .sol

--- DAY-2 setting number .sol
+++ DAY-2 setting number .sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Day2 {
+    //set number
     uint256 public number;
 
     function setNumber(uint256 _number) public {
@@ -10,5 +11,16 @@ contract Day2 {
 
     function getNumber() public view returns (uint256) {
         return number;
+    }
+
+    //mapping
+    mapping(uint256 => string) public map;
+
+    function setNameAndAge(string memory _name, uint256 id) public {
+        map[id] = _name;
+    }
+
+    function getNameById(uint256 id) public view returns (string memory) {
+        return map[id];
     }
 }
